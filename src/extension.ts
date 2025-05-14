1d236e2fbd404756ca10b3b1bc9e49743f82588e
Kristian Leonhard <kristian.leonhard@anticimex.com>
2025-05-14 21:44:59 +0200

    refactor: improve command registration and structure in extension activation

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { refreshConfig } from './configLoader';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,13 +14,23 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('devtwin-copilot-instruction-builder.helloWorld', () => {
+	const disposableHelloWorld = vscode.commands.registerCommand('devtwin-copilot-instruction-builder.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from DevTwin Copilot Instruction Builder!');
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposableHelloWorld);
+
+	let disposableRefreshConfig = vscode.commands.registerCommand('devtwin.refreshConfig', async () => {
+		try {
+			await refreshConfig();
+		} catch (err) {
+			// Error message already shown in refreshConfig
+		}
+	});
+
+	context.subscriptions.push(disposableRefreshConfig);
 }
 
 // This method is called when your extension is deactivated
